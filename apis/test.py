9da74186b462bdc822ed27c54724069a40241c92
Sébastien Sanscartier <ssans075@uottawa.ca>
2019-11-13 17:34:28 -0500

    renaming

--- apis/test.py
+++ apis/test.py
@@ -1,29 +1,24 @@
-import json
 import requests
-from pprint import pprint
 import json
 
 headers = { 'Content-Type': 'application/json'}
 
-def get_album():
+def get_team_stats():
 	api_url = 'https://statsapi.web.nhl.com/api/v1/teams/8/stats'
 	response = requests.get(api_url, headers=headers)
-
-	pprint(api_url)
-	pprint(response)
 
 	if response.status_code == 200:
 		return json.loads(response.content.decode('utf-8'))
 	else:
 		return None
 
-album_info = get_album()
+team_info = get_team_stats()
 
-if album_info is not None:
+if team_info is not None:
     print("Successfully created the .JSON file")
-    for album in album_info.items():
+    for team in team_info.items():
         with open('habs_team_info.json', 'w') as f:
-        	print(json.dumps(album, indent=2), file=f)
+        	print(json.dumps(team, indent=2), file=f)
 
 else:
         print('[!] Request Failed')
